b34f1cb2991850783d2fddca5a4d4d9297af6abd
Alexey <still.smoke@bk.ru>
2016-02-26 17:30:44 +0300

    add info

--- script/script.js
+++ script/script.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
 	$('#mainMenu ul li').hover(function(){
-								// $(this).addClass('animateButton');
-								$(this).animate({backgroundPosition : 'bottom 40px right 0% !important'}, 500)},
-							function(){
-								// $(this).removeClass('animateButton');
-								$(this).animate({backgroundPosition : 'bottom 130px right 0% !important'}, 500)
+									$(this).animate({
+										'backgroundPosition': 'center top'
+									}, 1000)},
+								function(){
+									$(this).animate({
+										'backgroundPosition': 'center bottom'
+									}, 1000)
 							});
 });

f1322af684fcb929d8102086ce5daab07d9daf6c
Alexey <still.smoke@bk.ru>
2016-02-27 22:26:44 +0300

    some changes

--- script/script.js
+++ script/script.js
@@ -8,4 +8,19 @@ $(document).ready(function() {
 										'backgroundPosition': 'center bottom'
 									}, 1000)
 							});
+
+	$('#social a').css('opacity', '0.5');
+	$('#social a').hover(function(){
+							$(this).stop().animate({opacity: '1'}, 1000)},
+						function(){
+							$(this).stop().animate({opacity: '0.5'}, 1000)});
+
+	$('#products div').fadeTo("slow", 0.6);
+	$('#products div').hover(function(){
+								$(this).stop().fadeTo("slow",1),
+								$(this).css('borderRadius', '5px')}, 
+							function(){
+								$(this).stop().fadeTo("slow", 0.6),
+								$(this).css('borderRadius', '')} 
+ 							); 
 });

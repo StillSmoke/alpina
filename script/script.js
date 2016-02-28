6d13efba1748249cf2552cb03eae8596b9c63f34
Alexey <still.smoke@bk.ru>
2016-02-28 15:04:49 +0300

    some changes

--- script/script.js
+++ script/script.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
 	$('#mainMenu ul li').hover(function(){
 									$(this).animate({
-										'backgroundPosition': 'center top'
+										'background-position': 'center top'
 									}, 1000)},
 								function(){
 									$(this).animate({
-										'backgroundPosition': 'center bottom'
+										'background-position': 'center bottom'
 									}, 1000)
 							});
 
@@ -15,12 +15,8 @@ $(document).ready(function() {
 						function(){
 							$(this).stop().animate({opacity: '0.5'}, 1000)});
 
-	$('#products div').fadeTo("slow", 0.6);
-	$('#products div').hover(function(){
-								$(this).stop().fadeTo("slow",1),
-								$(this).css('borderRadius', '5px')}, 
-							function(){
-								$(this).stop().fadeTo("slow", 0.6),
-								$(this).css('borderRadius', '')} 
- 							); 
+	$('#products div').click(function(){
+						$('#products div').stop().fadeOut(500, function(){$('#products span').load('product.html', 'x=' + $(this).attr('href'))})
+					}); 
+
 });
